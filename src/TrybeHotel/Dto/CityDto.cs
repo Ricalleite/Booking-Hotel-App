d4b79aa82d00ce44e9ed5bfe1325c909ab0947b5
Ricardo <ricalleite@gmail.com>
2023-09-04 15:40:50 -0300

    Começando o projeto...

--- src/TrybeHotel/Dto/CityDto.cs
+++ src/TrybeHotel/Dto/CityDto.cs
@@ -1,6 +1,6 @@
 namespace TrybeHotel.Dto {
 
-    //2. Refatore o endpoint POST /city
+    //2. Refatore o endpoint POST /city aqui
     //4. Refatore o endpoint GET /city
      public class CityDto {
     }
