a581daa20c1f76fe3dde90c330bcfd5c26543dc6
Ricardo <ricalleite@gmail.com>
2023-09-04 22:53:31 -0300

    Requisito 01 feito

--- src/TrybeHotel/Services/TokenGenerator.cs
+++ src/TrybeHotel/Services/TokenGenerator.cs
@@ -15,18 +15,37 @@ namespace TrybeHotel.Services
         {
            _tokenOptions = new TokenOptions {
                 Secret = "4d82a63bbdc67c1e4784ed6587f3730c",
-                ExpiresDay = 1
+                ExpiresDay = 7
            };
 
         }
         public string Generate(UserDto user)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = AddClaims(user),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_tokenOptions.Secret)),
+                SecurityAlgorithms.HmacSha256Signature
+                ),
+            Expires = DateTime.Now.AddDays(_tokenOptions.ExpiresDay)
+        };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
         private ClaimsIdentity AddClaims(UserDto user)
         {
-            throw new NotImplementedException();
+            var claimsAdmin = new ClaimsIdentity();
+            claimsAdmin.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+            if (user.UserType == "admin") 
+            {
+                claimsAdmin.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+            } 
+
+            return claimsAdmin;
         }
     }
 }
